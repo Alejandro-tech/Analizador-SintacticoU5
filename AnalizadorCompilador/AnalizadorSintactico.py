d918cfb02ea6828b3b1b459f73eb142f557bb0e6
Alejandro-tech <53318018+Alejandro-tech@users.noreply.github.com>
2021-01-26 18:43:28 -0600

    Add files via upload
    Mejoramiento del Analizador Sintáctico para que pueda recibir diferentes estructuras del lenguaje C#, además del diseño de un nuevo ejemplo

--- AnalizadorCompilador/AnalizadorSintactico.py
+++ AnalizadorCompilador/AnalizadorSintactico.py
@@ -5,9 +5,9 @@ import sys
 import ply.lex as lex 
 
 resultado_gramatica = []
-
+#ASIGNACIÓN DE LA PRECEDENCIA (IZQUIERDA DERECHA) CON MAYOR INDICE DE IMPORTANCIA
 precedence = (
-    ('right', 'IF','ELSE'),
+    ('right', 'IF','ELSE','FOR'),
     ('left', 'PARENTIZQ','PARENTDER'),
     ('right', 'PTCOMA'),
     ('left', 'SUMA', 'MENOS'),
@@ -18,6 +18,10 @@ precedence = (
 )
 
 nombres = {}
+#RECONOCIMIENTO PARA OPERACIONES BÁSICAS (DECLARADAS EN LA PRECENDENCIA)
+def p_declaracion_asignar(t):
+    'declaracion :  TIPO ID IGUALQUE expresion PTCOMA'
+    nombres[t[1]] = t[3]
 
 def p_declaracion_coditionif(t):
     'declaracion : IF'
@@ -39,7 +43,7 @@ def p_declaracion_expr(t):
     'declaracion : expresion'
     t[0] = t[1]
 
-
+#RECONOCIMIENTO DE LAS EXPRESIONES BÁSICAS Y ASIGNACIÓN DE POSICIONES EN LA ENTRADA
 def p_expresion_operaciones(t):
     '''
     expresion  :   expresion SUMA expresion
@@ -56,13 +60,20 @@ def p_expresion_operaciones(t):
     if t[2] == '/':
         t[0] = t[1] + t[3]
 
+#RECONOCIMIENTO DE PARENTESIS
 def p_expresion_grupo(t):
     '''
     expresion  : PARENTIZQ expresion PARENTDER
                 | CORCHETEIZQ expresion CORCHETEDER
     '''
     t[0] = t[2]
-
+#RECONOCIMEINTO DE LOS BUCLES IF ELSE Y FOR
+def p_expresion_bucle(t):
+    '''
+    expresion : IF PARENTIZQ expresion MENORIGUALQUE expresion PARENTDER PARENTIZQ expresion IGUALIGUAL expresion PTCOMA PARENTDER ELSE PARENTIZQ expresion IGUALIGUAL expresion PTCOMA PARENTDER
+                |  FOR PARENTIZQ expresion IGUALQUE expresion PTCOMA expresion MAYORQUE expresion PTCOMA expresion MASMAS PARENTDER PARENTIZQ ID PARENTDER
+    '''
+#RECONOCIMIENTO DE LAS OPERACIONES LÓGICAS Y SU POSICIÓN
 def p_expresion_logicas(t):
     '''
     expresion   :  expresion MENORQUE expresion 
@@ -70,8 +81,8 @@ def p_expresion_logicas(t):
                 |  expresion MENORIGUALQUE expresion 
                 |   expresion MAYORIGUALQUE expresion 
                 |   expresion IGUALQUE expresion 
-                |   PARENTIZQ expresion MENORQUE expresion PARENTDER
-                |   IF PARENTIZQ expresion MAYORQUE expresion PARENTDER
+                |   IF PARENTIZQ expresion MENORQUE expresion PARENTDER CORCHETEIZQ expresion PTCOMA CORCHETEDER
+                |   IF PARENTIZQ expresion MAYORQUE expresion PARENTDER CORCHETEIZQ expresion PTCOMA CORCHETEDER
                 |   PARENTIZQ expresion MENORIGUALQUE expresion PARENTDER
                 |   PARENTIZQ expresion MAYORIGUALQUE expresion PARENTDER
                 |   PARENTIZQ expresion IGUALQUE expresion PARENTDER
@@ -98,34 +109,57 @@ def p_expresion_logicas(t):
     elif t[3] == "=":
         t[0] = t[2] is t[4]
     
-def p_expresion_numero(t):
-    'expresion : NUMBER'
-    t[0] = t[1]
-
+#DECLARACIÓN DE ALGUNAS ESTRUCTURAS DE RECONOCIMIENTO ESPECIFICO    
 def p_expresion_int(t):
-    'expresion : INT ID IGUALQUE NUMBER PTCOMA'
+    'expresion : TIPO ID IGUALQUE NUMBER PTCOMA'
     t[0] = t[1]
 
-def p_expresion_intd(t):
-    'expresion : INT ID PTCOMA'
-    t[0] = t[1]
 
-def p_expresion_intt(t):
-    'expresion : INT ID IGUALQUE ID MULTIPLICAR ID PTCOMA'
-    t[0] = t[1]
-
-def p_expresion_intc(t):
-    'expresion : INT ID IGUALQUE ID PTCOMA'
+def p_expresion_deci(t):
+    'expresion : TIPO ID IGUALQUE DECIMAL SUMA DECIMAL PTCOMA'
     t[0] = t[1]
 
 def p_expresion_id(t):
     'expresion : ID'
     t[0] = t[1]
 
-def p_expresion_end(t):
-    'expresion : END'
+def p_expresion_cori(t):
+    'expresion : CORCHETEIZQ'
     t[0] = t[1]
 
+def p_expresion_cord(t):
+    'expresion : CORCHETEDER'
+    t[0] = t[1]
+
+def p_expresion_cont(t):
+    'expresion : ID IGUALQUE ID SUMA NUMBER PTCOMA'
+    t[0] = t[1]
+
+def p_expresion_intd(t):
+    'expresion : TIPO ID PTCOMA'
+    t[0] = t[1]
+
+def p_expresion_intm(t):
+    'expresion : TIPO ID IGUALQUE ID MENOS NUMBER PTCOMA'
+    t[0] = t[1]
+
+def p_expresion_intt(t):
+    'expresion : TIPO ID IGUALQUE ID MULTIPLICAR ID PTCOMA'
+    t[0] = t[1]
+
+def p_expresion_intc(t):
+    'expresion : TIPO ID IGUALQUE ID PTCOMA'
+    t[0] = t[1]
+
+def p_expresion_number(t):
+    'expresion : NUMBER'
+    t[0] = t[1]
+
+def p_expresion_dec(t):
+    'expresion : INT'
+    t[0] = t[1]
+
+#CON LA EXISTENCIA DE ERROR SE PRESENTA EL TIPO DE ERROR
 def p_error(t):
     global resultado_gramatica
     if t:
@@ -136,7 +170,7 @@ def p_error(t):
     resultado_gramatica.append(resultado)
 
 parser = yacc.yacc()
-
+#IMPRIMIR LOS RESULTADOS
 def prueba_sintactica(data):
     global resultado_gramatica
    
@@ -162,8 +196,6 @@ for linea in archivo:
 
 prueba_sintactica(text)
 print("")
-print('>>>>> COMPROBACIÓN DE ERRORES <<<<<')
-print('>>>>> NONE MARCA QUE NO EXISTE ERRORES <<<<<')
+print('----PRUEBA----')
 print("")
 print('\n'.join(list(map(''.join, resultado_gramatica))))
-print('-------------------------------------------------')
--- AnalizadorCompilador/Prueba4.cs
+++ AnalizadorCompilador/Prueba4.cs
@@ -1,12 +1,4 @@
-int a = 9;
-int c = 12;
-int k = 23;
-int p;
 
-if (a > c) {
 
-    int b = k * p;
 
-    int g = a / c;
-}
-
+double operac = (2.5 * 9.4) / 2;
